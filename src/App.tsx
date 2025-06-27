993a7efe81a02594d8686f699272d9c73a3abe94
LHH2904 <102815563+LHH2904@users.noreply.github.com>
2025-06-27 18:12:41 +0700

    Create page and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 
-import { Button } from "flowbite-react";
+
 function App() {
-
-
   return (
-    <>
-        <Button>Click me</Button>
-        <h1 className='text-2xl text-red-500'>App</h1>
-    </>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/sign-in" element={<SignIn/>} />
+              <Route path="/sign-up" element={<SignUp/>} />
+              <Route path="/about" element={<About/>} />
+              <Route path="/profile" element={<Profile/>} />
+          </Routes>
+      </BrowserRouter>
   )
 }
 
---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,11 @@
+
+
+const SignUp = () => {
+    return (
+        <div>
+            <h2>Logn out</h2>
+        </div>
+    );
+};
+
+export default SignUp;
