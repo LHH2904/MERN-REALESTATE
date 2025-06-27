24f0686676fa8367abc2663081704e32bc8f1064
LHH2904 <102815563+LHH2904@users.noreply.github.com>
2025-06-27 17:56:40 +0700

    create project and install reactjs, typescript, flowbite-react, tailwind

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+import { Button } from "flowbite-react";
+function App() {
+
+
+  return (
+    <>
+        <Button>Click me</Button>
+        <h1 className='text-2xl text-red-500'>App</h1>
+    </>
+  )
+}
+
+export default App
