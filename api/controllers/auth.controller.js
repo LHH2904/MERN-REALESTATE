2f427d0ffcac40f97059b79813b5cdc4c26d16ad
LHH2904 <102815563+LHH2904@users.noreply.github.com>
2025-06-28 11:17:43 +0700

    create a middleware and a function to handle possible errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
 // Định nghĩa hàm signup để xử lý đăng ký người dùng mới
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     // Lấy dữ liệu từ body của request gửi từ client (gồm username, email và password)
     const { username, email, password } = req.body;
 
@@ -24,8 +24,6 @@ export const signup = async (req, res) => {
         res.status(201).json("User successfully created!");
     } catch (error) {
         // Nếu xảy ra lỗi (ví dụ: trùng email, lỗi kết nối DB...), gửi mã lỗi 500
-        res.status(500).json(error.message);
+        next(error);
     }
-
-
 };
