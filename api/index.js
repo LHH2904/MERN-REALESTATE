4cbeb7fcbcfaa85594cef021fe2d4f550ece90ab
LHH2904 <102815563+LHH2904@users.noreply.github.com>
2025-06-30 18:16:53 +0700

    create user update api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -17,11 +18,13 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.listen(3000, () => {
     console.log('Server started on port 3000!');
 });
 
-app.use("/api/users", userRoute);
+app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 
 app.use((err,req, res, next) => {
